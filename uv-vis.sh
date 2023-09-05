2d1ac5f483bd78e482686d6bc027836a227f9e78
EmanuelContC <124008173+EmanuelContC@users.noreply.github.com>
2023-09-05 13:16:44 -0600

    Update uv-vis.sh

--- uv-vis.sh
+++ uv-vis.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/joaquinbarroso/UVeh-VIs/main/ReadES2.py
-curl -O https://raw.githubusercontent.com/joaquinbarroso/UVeh-VIs/main/Plot.py
 
-chmod +x Plot.py ReadES2.py
+chmod +x Plotter.py
 
-python3 Plot.py
+python3 Plotter.py
 
-rm Plot.py ReadES2.py
