6588fd1dd313f332796cd99df4742a70a683a465
Emanuel Contreras <124008173+EmanuelContC@users.noreply.github.com>
2024-01-19 14:32:36 -0600

    Plotter style corrections and xlabel

--- Plotter.py
+++ Plotter.py
@@ -2,12 +2,16 @@
 import matplotlib.pyplot as plt
 import math as math
 a=len(datas)+1
+lambda1=int(lambda1)
+lambda2=int(lambda2)
+plt.figure(figsize=(10,6))
+plt.rcParams.update({'font.size': 13})
 for f in range (1,a):
         exec("nstates_"+str(f)+"=len(nm_values_"+str(f)+""")            
 ABS_"""+str(f)+" = []")
         wavelenght=[]
         deviation=(float(dev)*ev_to_joules*1e7)/(h*c)
-        for i in range(int(lambda1),int(lambda2)+1):
+        for i in range(lambda1,lambda2):
                 wavelenght.append(i)
                 exec("ABSi_"+str(f)+"=0")
                 exec("for n in range(0,nstates_"+str(f)+"""):
@@ -18,7 +22,9 @@ ABS_"""+str(f)+" = []")
         exec("name=datas["+str(f-1)+"""].split(".")[0]
 plt.plot(wavelenght,ABS_"""+str(f)+",label=name)")
 csv = args.table
-plt.legend()
+plt.legend(edgecolor='None')
+plt.xlabel('Wavelenght /nm', size=15)
+plt.xlim(lambda1,lambda2)
 if csv:
         csvPATH=PATH+'/gen_csv'
         with open(csvPATH) as f:  #15/01/24
