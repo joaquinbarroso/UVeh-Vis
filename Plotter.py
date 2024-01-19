be3f187b886bbdc22e4dda3941c0a139bdde4016
Emanuel Contreras <124008173+EmanuelContC@users.noreply.github.com>
2024-01-19 17:31:36 -0600

    Lorentzian curves added!!
    OMG that was hard

--- Plotter.py
+++ Plotter.py
@@ -4,21 +4,33 @@ import math as math
 a=len(datas)+1
 lambda1=int(lambda1)
 lambda2=int(lambda2)
-plt.figure(figsize=(10,6))
+plt.figure(figsize=(12,6))
 plt.rcParams.update({'font.size': 13})
+ltz = args.lorentzian
 for f in range (1,a):
         exec("nstates_"+str(f)+"=len(nm_values_"+str(f)+""")            
 ABS_"""+str(f)+" = []")
         wavelenght=[]
         deviation=(float(dev)*ev_to_joules*1e7)/(h*c)
-        for i in range(lambda1,lambda2):
-                wavelenght.append(i)
-                exec("ABSi_"+str(f)+"=0")
-                exec("for n in range(0,nstates_"+str(f)+"""):
-                        exp=-1*(((1/i)-(1/nm_values_"""+str(f)+"""[n]))/(deviation/1e7))**2
-                        ABSn=1.3062974e8*(osc_values_"""+str(f)+"""[n]/deviation)*math.exp(exp)
-                        ABSi_"""+str(f)+"+=ABSn")
-                exec("ABS_"+str(f)+".append(ABSi_"+str(f)+")")
+        if ltz:
+                for i in range(lambda1,lambda2+1):
+                        wavelenght.append(i)
+                        exec("ABSi_"+str(f)+"=0")
+                        exec("for n in range(0,nstates_"+str(f)+"""):
+                                sup=1.3062974e8*(2/math.pi)**0.5*osc_values_"""+str(f)+"""[n]*(deviation/1e7)
+                                inf=((1/i)-(1/nm_values_"""+str(f)+"""[n]))**2+(deviation/1e7)**2
+                                ABSn=sup/inf
+                                ABSi_"""+str(f)+"+=ABSn")
+                        exec("ABS_"+str(f)+".append(ABSi_"+str(f)+")")
+        else:
+                for i in range(lambda1,lambda2+1):
+                        wavelenght.append(i)
+                        exec("ABSi_"+str(f)+"=0")
+                        exec("for n in range(0,nstates_"+str(f)+"""):
+                                exp=-1*(((1/i)-(1/nm_values_"""+str(f)+"""[n]))/(deviation/1e7))**2
+                                ABSn=1.3062974e8*(osc_values_"""+str(f)+"""[n]/deviation)*math.exp(exp)
+                                ABSi_"""+str(f)+"+=ABSn")
+                        exec("ABS_"+str(f)+".append(ABSi_"+str(f)+")")
         exec("name=datas["+str(f-1)+"""].split(".")[0]
 plt.plot(wavelenght,ABS_"""+str(f)+",label=name)")
 csv = args.table
